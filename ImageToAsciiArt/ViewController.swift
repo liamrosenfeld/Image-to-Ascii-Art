f7529f72dde21eeb3a0f0561fde4b81db7eb2979
Liam Rosenfeld <liamrosenfeld@gmail.com>
2017-11-11 19:10:59 -0500

    It's a start

--- ImageToAsciiArt/ViewController.swift
+++ ImageToAsciiArt/ViewController.swift
@@ -11,22 +11,16 @@ import UIKit
 class ViewController:
     UIViewController,
     UIImagePickerControllerDelegate,
-    UINavigationControllerDelegate, // required by image picker
-    UIScrollViewDelegate
+    UINavigationControllerDelegate // required by image picker
 {
     fileprivate let labelFont = UIFont(name: "Menlo", size: 7)!
     fileprivate let maxImageSize = CGSize(width: 310, height: 310)
     fileprivate lazy var palette: AsciiPalette = AsciiPalette(font: self.labelFont)
     
-    fileprivate var currentLabel: UILabel?
-    @IBOutlet weak var busyView: UIView!
-    @IBOutlet weak var scrollView: UIScrollView!
-    
     // Setup
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // self.configureZoomSupport()
     }
     
     // Actions
@@ -51,12 +45,6 @@ class ViewController:
         self.performSegue(withIdentifier: "homeToContent", sender: self)
         displayImageNamed("batman")
     }
-    
-    @IBAction func handleNewImageTapped(_ sender: AnyObject) {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        self.show(imagePicker, sender: self)
-    }
 
     // UIImagePickerControllerDelegate
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
@@ -72,79 +60,5 @@ class ViewController:
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController)
     {
         self.dismiss(animated: true, completion: nil)
-    }
-    
-    // MARK: - Rendering
-    fileprivate func displayImageNamed(_ imageName: String)
-    {
-        displayImage(UIImage(named: imageName)!)
-    }
-    
-    fileprivate func displayImage(_ image: UIImage)
-    {
-        self.busyView.isHidden = false
-        DispatchQueue.global(qos: DispatchQoS.QoSClass.background).async {
-            
-            let // Rotate first because the orientation is lost when resizing.
-            rotatedImage = image.imageRotatedToPortraitOrientation(),
-            resizedImage = rotatedImage.imageConstrainedToMaxSize(self.maxImageSize),
-            asciiArtist  = AsciiArtist(resizedImage, self.palette),
-            asciiArt     = asciiArtist.createAsciiArt()
-            
-            DispatchQueue.main.async {
-                self.displayAsciiArt(asciiArt)
-                self.busyView.isHidden = true
-            }
-            
-            print(asciiArt)
-            UIPasteboard.general.string = asciiArt
-
-        }
-    }
-    
-    fileprivate func displayAsciiArt(_ asciiArt: String)
-    {
-        let label = UILabel()
-        label.font = self.labelFont
-        label.lineBreakMode = NSLineBreakMode.byClipping
-        label.numberOfLines = 0
-        label.text = asciiArt
-        label.sizeToFit()
-        
-        currentLabel?.removeFromSuperview()
-        currentLabel = label
-        
-        scrollView.addSubview(label)
-        scrollView.contentSize = label.frame.size
-        
-        self.updateZoomSettings(animated: false)
-        scrollView.contentOffset = CGPoint.zero
-        
-        
-    }
-    
-    // Zooming support
-    fileprivate func configureZoomSupport()
-    {
-        scrollView.delegate = self
-        scrollView.maximumZoomScale = 5
-    }
-    
-    fileprivate func updateZoomSettings(animated: Bool)
-    {
-        let
-        scrollSize  = scrollView.frame.size,
-        contentSize = scrollView.contentSize,
-        scaleWidth  = scrollSize.width / contentSize.width,
-        scaleHeight = scrollSize.height / contentSize.height,
-        scale       = max(scaleWidth, scaleHeight)
-        scrollView.minimumZoomScale = scale
-        scrollView.setZoomScale(scale, animated: animated)
-    }
-    
-    // UIScrollViewDelegate
-    func viewForZooming(in scrollView: UIScrollView) -> UIView?
-    {
-        return currentLabel
     }
 }
