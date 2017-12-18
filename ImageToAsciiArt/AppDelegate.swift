e32032c320eea016c6bd5c82e9bffa6ca2bcd903
Liam Rosenfeld <liamrosenfeld@gmail.com>
2017-12-18 17:18:33 -0500

    Added firebase to main app
    For analytics

--- ImageToAsciiArt/AppDelegate.swift
+++ ImageToAsciiArt/AppDelegate.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
     var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
 }
